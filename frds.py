98a7bcd18ca336c4bddfb338191b1fa491060fd1
friendship777 <76768546+friendship777@users.noreply.github.com>
2021-01-10 13:05:36 +0900

    Update frds.py

--- frds.py
+++ frds.py
@@ -33,7 +33,7 @@ async def on_ready():
         game = discord.Game("서버유저 관리")
         await client.change_presence(status=discord.Status.dnd, activity=game)
         await asyncio.sleep(3)
-        game = discord.Game("https://discord.me/frdshub")
+        game = discord.Game("https://discord.gg/nEzPCjd")
         await client.change_presence(status=discord.Status.dnd, activity=game)
         await asyncio.sleep(3)
         game = discord.Game("서버문의: 우정#9444 ")
